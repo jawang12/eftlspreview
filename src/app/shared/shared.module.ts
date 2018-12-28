a800fc32a488b66c7074c7175a7952211be1314c
Jimmy Wang <jawang12@gmail.com>
2018-12-27 19:54:55 -0500

    initialization

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { MaterialModule } from './material.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  imports: [CommonModule, MaterialModule, FlexLayoutModule],
+  exports: [CommonModule, MaterialModule, FlexLayoutModule]
+})
+export class SharedModule {}
